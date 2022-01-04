80809c0fdf397dc117218790f3a9d01d2c8c1253
ihsan <ihsankaandelice07@gmail.com>
2022-01-04 09:23:36 +0300

    yazım yanlışları

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,7 +22,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Şartları kabul etmelisiniz.',
                     ]),
                 ],
             ])
@@ -33,11 +33,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Parola giriniz',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Parolanız en az 6 karakter olmalıdır',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
